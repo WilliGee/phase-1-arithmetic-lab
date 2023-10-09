d1a075d1c91648da2587cac28d841b18fba2cb5a
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2023-10-09 20:10:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+var multiply = (num1 * num2)
+let num3 = 24;
+let num4 = 5;
+var mod = (num3 % num4)
+var max = (10, 15, 20);
+var random = Number.isInteger() * 10 + 1;
